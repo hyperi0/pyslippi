e4ecf1aab23fd8b84ff7a7ba0dbc54be1c188ac2
Ben <ben.jacobsoh07@gmail.com>
2021-10-25 19:04:06 -0400

    not much

---
+++ findMatchup.py
@@ -0,0 +1,25 @@
+import slippi
+from slippi import Game
+
+def __init__():
+
+    this.args = sys.argv[1:]
+    this.baseDir = "."
+    this.tag = "DAB#823"
+    this.myChar = ""
+    this.vsChar = ""
+
+    try i = args.index("-d"):
+        baseDir = args[i+1]
+    except ValueError:
+        baseDir = "."
+
+    try i = args.index("m"):
+        myChar = args[i+1]
+        vsChar = args[i+2]
+    except ValueError:
+        myChar = args[0]
+        vsChar = args[1]
+
+if __name__ == '__main__':
+    sys.exit(main())
